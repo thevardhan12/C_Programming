39b7b93d9a90858cd718efb2fac0f92cded70624
thevardhan12 <harshavd06@gmail.com>
2023-02-19 21:58:00 +0530

    commit

--- practice/Array/max_mini_of_array.c
+++ practice/Array/max_mini_of_array.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int arr={7,6,8,2,77,7,16,5,63};
-    int max=arr[0];
-    int mini=arr[0];
+    int max,mini;
+    int arr[9]={7,6,8,2,77,7,16,5,63};
+    max=mini=arr[0];
     for (int  i = 0; i < 9; i++)
     {
         if (arr[i]<mini)
@@ -17,5 +17,8 @@ int main()
             /* code */
         }
     }
+    printf("%d\n",max);
+    printf("%d\n",mini);
+
     
 }
