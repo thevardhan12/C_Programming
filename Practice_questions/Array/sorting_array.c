bfa182c001055b8a7a58ca42dd4428693729cc0a
thevardhan12 <harshavd06@gmail.com>
2023-02-21 21:33:44 +0530

    commit

--- Practice_questions/Array/sorting_array.c
+++ Practice_questions/Array/sorting_array.c
@@ -4,12 +4,50 @@ int linear_sort(int *arr,int);
 
 int main()
 {
-    int arr[10];
-    for (int i = 0; i < 10; i++)
+    int size;
+    printf("enter a size\n");
+    scanf("%d",&size);
+    int arr[size];
+    printf("Enter a an array\n");
+    for (int i = 0; i < size; i++)
     {
-        scanf("%d",arr[i]);
+        scanf("%d",&arr[i]);
         /* code */
     }
+    bubble_sort(arr,size);
+    
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",arr[i]);
+        /* code */
+    }
+     
+}
+int bubble_sort(int *arr,int size)
+{
+    int temp;
+    for (int  i = 0; i < size; i++)
+    {
+        for (int  j = 0; j < size-i-1; i++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                
+            }
+            
+            
+        }
+        
+        
+    }
+    
 
+}
+/*int linear_sort(int *arr,int size)
+{
     
 }
+*/
