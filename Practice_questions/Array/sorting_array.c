79da2afa486c9b28417ca14fe862b09958583520
thevardhan12 <harshavd06@gmail.com>
2023-02-21 08:01:52 +0530

    commit

---
+++ Practice_questions/Array/sorting_array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int bubble_sort(int *arr,int);
+int linear_sort(int *arr,int);
+
+int main()
+{
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d",arr[i]);
+        /* code */
+    }
+
+    
+}
