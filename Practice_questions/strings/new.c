3891664ffe386f33a852bf625664eac536c125fb
thevardhan12 <harshavd06@gmail.com>
2023-02-27 21:01:19 +0530

    commit

---
+++ Practice_questions/strings/new.c
@@ -0,0 +1,30 @@
+//Write a program to accept a line of text and a word. Display the number of occurrences of that
+//word in the text.
+#include<stdio.h>
+#include<string.h>
+int  occurence_of_word(char*,char*);
+int main()
+{
+    char ch1;
+    char str[50];
+    char str2[10];
+    printf("ENter a string\n");
+    scanf("%[^\n]",str);
+    printf("enter a word\n");
+    scanf("%s",str2);
+    occurence_of_word(str,str2);
+}
+int  occurence_of_word(char *str1,char *str2)
+{
+    for (int  i = 0; str1[i]!=0; i++)
+    {
+        for (int j = 0; str2[j]; j++)
+        {
+            str1[]
+        }
+        
+    }
+    
+
+}
+
