506b474c21621a2c46e1678aa513ab2e9fe2f176
thevardhan12 <harshavd06@gmail.com>
2023-02-28 09:30:52 +0530

    commit

--- Practice_questions/strings/my_strdup.c
+++ Practice_questions/strings/my_strdup.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
+char *my_strdup(char *str,char *);
 int main()
 {
     char str[10];
     printf("Enter a string\n");
     scanf("%s",str);
+    char *s=calloc(20,sizeof(char));
+    my_strdup(str,s);
+    printf("%s\n",s);
     
-
+}
+char *my_strdup(char *str,char *s)
+{
+    //char *s=calloc(20,sizeof(char));
+    for (int  i = 0;str[i]!=0; i++)
+    {
+        s[i]=str[i];
+    }
+    
 }
